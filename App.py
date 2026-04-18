106b011a1365b2bbe624c6651510a8562f42dd22
Kushal-M-Chakrasali <chakrasalikushal@gmail.com>
2026-04-18 10:17:03 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,40 +1,29 @@
 import streamlit as st
-from diffusers import StableDiffusionPipeline
-import torch
+import os
+from huggingface_hub import InferenceClient
 from PIL import Image
 
-# Page configuration
+# Set page config
 st.set_page_config(page_title="KMC AI Text to Image", layout="centered")
 
-# Display Logo
+# Logo
 logo = Image.open("KMC_Transperent.jpeg")
 st.image(logo, width=250)
 
 st.title("KMC AI - Text to Image Generator")
 
-st.write("Generate images from text prompts using Stable Diffusion.")
-
 # Prompt input
 prompt = st.text_input("Enter your prompt")
 
-# Load lightweight model
-@st.cache_resource
-def load_model():
-    model_id = "stabilityai/sd-turbo"
+# Hugging Face token
+HF_TOKEN = os.getenv("HF_TOKEN")
 
-    pipe = StableDiffusionPipeline.from_pretrained(
-        model_id,
-        torch_dtype=torch.float32
-    )
+# Create client
+client = InferenceClient(
+    provider="fal-ai",
+    api_key=HF_TOKEN,
+)
 
-    pipe = pipe.to("cpu")
-
-    return pipe
-
-
-pipe = load_model()
-
-# Generate button
 if st.button("Generate Image"):
 
     if prompt.strip() == "":
@@ -42,18 +31,22 @@ if st.button("Generate Image"):
 
     else:
         with st.spinner("Generating image..."):
-            image = pipe(prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
 
-        st.image(image, caption="Generated Image", use_container_width=True)
-
-        # Save image
-        image.save("generated_image.png")
-
-        # Download button
-        with open("generated_image.png", "rb") as file:
-            st.download_button(
-                label="Download Image",
-                data=file,
-                file_name="generated_image.png",
-                mime="image/png"
+            image = client.text_to_image(
+                prompt,
+                model="baidu/ERNIE-Image",
             )
+
+            st.image(image, caption="Generated Image", use_container_width=True)
+
+            # Save image
+            image.save("generated_image.png")
+
+            # Download button
+            with open("generated_image.png", "rb") as file:
+                st.download_button(
+                    label="Download Image",
+                    data=file,
+                    file_name="generated_image.png",
+                    mime="image/png"
+                )
